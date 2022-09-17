5541fed229593b52984f7075ef6dc9baf8a84de7
Higor Emanuel <higoremanuel.r@gmail.com>
2022-09-17 10:53:02 -0300

    Created the Department and Seller classes, and the DepartmentDao and SellerDao interfaces

--- src/demo/dao/jdbc/Program.java
+++ src/demo/dao/jdbc/Program.java
@@ -1,9 +1,16 @@
 package demo.dao.jdbc;
 
+import java.util.Date;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
     public static void main(String[] args) {
         
-    }
-    
+        Department department = new Department(01, "MSW");
+        Seller sl = new Seller(20, "Higor", "higor@gmail.com", new Date(), 2000.0, department);
+        
+        System.out.println(sl);
+    }  
 }
